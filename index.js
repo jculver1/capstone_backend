fd6b87bbe4c34366709c74ba363d2ba62fc6128f
jculver1 <jculver1900@gmail.com>
2019-04-05 12:32:46 -0600

    added heroku

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 var cors = require('cors')
 const parser = require('body-parser')
 const knex = require('./knex');
 
+require('dotenv').config() 
+
 app.use(cors())
 app.use(parser.json())
+
 
 app.get('/foods/:char', (req, res) => {
   knex('foods').where('name', `${req.params.char}`)
--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://localhost/eatit'
+    connection: process.env.DATABASE_URL || "postgres://irntvpknatfvem:8d494b097a55b9275b51981e864a9720deebb31501d3d17cfed348587bda9e41@ec2-54-225-95-183.compute-1.amazonaws.com:5432/dbtu62s3ubjorc"
   }
 
 }
