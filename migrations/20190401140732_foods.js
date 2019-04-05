71669d1b023a68e9b47eb9a37c6f34f537027147
jculver1 <jculver1900@gmail.com>
2019-04-05 13:25:47 -0600

    migrations and seeds

--- migrations/20190401140732_foods.js
+++ migrations/20190401140732_foods.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('foods', (table)=>{
-        table.integer('usda_id')
+        table.string('usda_id')
         table.string('name').notNullable().defaultTo('')
     })
 };
